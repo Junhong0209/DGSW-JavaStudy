b13727062d860e425b99793119319b960622a087
Junhong0209 <junh040209@naver.com>
2021-06-01 14:21:55 +0900

    2021.6.1 수업 내용

---
+++ DGSW_2/src/kr/hs/dgsw/java/c2/jdbc/DriverTest.java
@@ -0,0 +1,19 @@
+package kr.hs.dgsw.java.c2.jdbc;
+
+public class DriverTest {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			Class.forName("org.mariadb.jdbc.Driver");
+			System.out.println("Success");
+		
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		
+		}
+	}
+	
+}
