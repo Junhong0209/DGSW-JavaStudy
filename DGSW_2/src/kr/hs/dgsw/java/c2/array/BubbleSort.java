85ca69d0129d10822c447cc4ae89a63a090b1519
Junhong0209 <junh040209@naver.com>
2021-06-01 14:19:47 +0900

    2021.4.14 수업 내용

---
+++ DGSW_2/src/kr/hs/dgsw/java/c2/array/BubbleSort.java
@@ -0,0 +1,31 @@
+package kr.hs.dgsw.java.c2.array;
+
+public class BubbleSort extends Sort {
+	
+	@Override
+	public void sort() {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; i++) {
+				if 
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Sort sort = new BubbleSort();
+		
+		sort.fillValues();
+		
+		System.out.println("정렬 전");
+		sort.printArray();
+		
+		long before = System.currentTimeMillis();
+		sort.sort();
+		long after = System.currentTimeMillis();
+		
+		System.out.println("정렬 후");
+		sort.printArray();
+		
+		System.out.println("경과 시간: " + (after - before));
+	}
+}
